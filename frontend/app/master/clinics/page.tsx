2added0b220830334a4ddbc037e6d57370b92dc2
Luiz Alberto <luizqsinclusao@gmail.com>
2025-12-27 09:47:31 -0300

    fix(build): add missing useEffect import in master/clinics

--- frontend/app/master/clinics/page.tsx
+++ frontend/app/master/clinics/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 // Mock data for clinics since we don't have a "List Clinics" endpoint yet

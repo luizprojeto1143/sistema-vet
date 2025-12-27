d35fab3d0f61c3c4e02b99d674f9e0f7a21fc910
Luiz Alberto <luizqsinclusao@gmail.com>
2025-12-27 15:39:53 -0300

    fix(backend): import StockModule in MedicalRecordsModule to resolve DI error

--- backend/src/medical-records/medical-records.module.ts
+++ backend/src/medical-records/medical-records.module.ts
@@ -8,8 +8,10 @@ import { ProductsModule } from '../products/products.module';
 import { FinanceModule } from '../finance/finance.module';
 import { AppointmentsModule } from '../appointments/appointments.module';
 
+import { StockModule } from '../stock/stock.module';
+
 @Module({
-    imports: [PrismaModule, ProductsModule, FinanceModule, AppointmentsModule],
+    imports: [PrismaModule, ProductsModule, FinanceModule, AppointmentsModule, StockModule],
     controllers: [MedicalRecordsController],
     providers: [MedicalRecordsService, AutoLockService],
 })
